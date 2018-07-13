925e99db62c8ca93cdf9266664abc037dbe37e6b
norotaro <nelson.otazo@gmail.com>
2018-07-13 14:59:06 -0400

    fix drawer import of store file

--- template/src/components/Drawer.vue
+++ template/src/components/Drawer.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-navigation-drawer right temporary fixed app v-model="app.drawer.show">
+  <v-navigation-drawer right temporary fixed app v-model="store.app.drawer.show">
     <v-card flat color="primary" height="200px">
       
     </v-card>
     <v-list>
-      <template v-for="item in app.drawer.items">
+      <template v-for="item in store.app.drawer.items">
         <v-list-tile v-if="item.type == null || item.type === 'tile'" :to="item.to">
           <v-list-tile-action> <v-icon v-text="item.icon"></v-icon> </v-list-tile-action>
           <v-list-tile-content>
@@ -19,13 +19,13 @@
 </template>
 
 <script>
-import app from '@/store/app'
+import store from '@/store'
 
 export default {
   name: 'drawer',
   data () {
     return {
-      app
+      store
     }
   }
 }
